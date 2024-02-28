7b01f119063b91c9b85a1e552596c422e214c507
Demian_Doru <demian.doru@outlook.com>
2024-02-28 06:00:57 +0200

    Added controller, envinronmet link and service for comunication with controller

--- SuperHeroUI/src/app/services/super-hero.service.ts
+++ SuperHeroUI/src/app/services/super-hero.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@angular/core';
 import { SuperHero } from '../models/super-hero';
-// import { HttpClient } from 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 @Injectable({
     providedIn: 'root'
 })
 export class SuperHeroService {
+    url = "SuperHero";
 
-    constructor(
-        // httpClient: HttpClient
-    ) { }
+    constructor(private http: HttpClient) { }
 
-    public getSuperHeroes(): SuperHero[] {
-        let hero = new SuperHero();
-        hero.id = 12;
-        hero.firstName = "Spiederman";
-        hero.lastName = "Venom";
-        hero.name = "Peter Parker";
-        hero.place = "New York";
-
-        return [hero];
+    public getSuperHeroes(): Observable<SuperHero[]> {
+        return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
     }
 }
